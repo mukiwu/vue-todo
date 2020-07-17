135bb6930044871b3493f373b800dd25a5cda0a0
muki wu <mukispace@gmail.com>
2020-07-17 10:43:51 +0800

    搬移 css 檔案

--- src/App.vue
+++ src/App.vue
@@ -14,46 +14,8 @@
   </div>
 </template>
 
-<style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-  background: #e1e1e1;
-	font-family: arial, sans-serif;
-	font-size: 16px;
-	line-height: 160%;
-}
+<script>
+import '@/assets/css/app.scss'
 
-section {
-  width: 620px;
-	margin: 0 auto;
-}
-
-nav {
-  background: #4a90e2;
-	font-size: 24px;
-	color: #00408b;
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-  li {
-    display:inline-block
-  }
-	a {
-		display: inline-block;
-    color: #00408b;
-		text-align: center;
-		padding: 24px 0 19px 0;
-		border-bottom: 5px solid #4a90e2;
-		width: 174px;
-		margin: 0 10px;
-    text-decoration: none;
-		&.router-link-exact-active, &:hover {
-			color: #fff;
-			cursor: pointer;
-			border-bottom: 5px solid #00408b;
-		}
-	}
-}
-</style>
+export default {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
     <el-form ref="form" :model="form" label-width="80px">
-      <div class="main__input">
+      <div class="form__input">
         <input type="text" placeholder="+ 新增一個待辦事項" required v-if="!todoShow" @click="todoShow=true">
         <input type="text" placeholder="請在此輸入標題" v-model="form.title" v-if="todoShow">
       </div>
-      <div class="main__content" v-if="todoShow">
+      <div class="form__content" v-if="todoShow">
         <el-form-item label="到期日">
           <el-date-picker type="datetime" placeholder="選擇日期時間" v-model="form.deadline" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
         </el-form-item>
@@ -13,7 +13,7 @@
           <el-input type="textarea" v-model="form.content"></el-input>
         </el-form-item>
       </div>
-      <div class="main__action" v-if="todoShow">
+      <div class="form__action" v-if="todoShow">
         <el-button-group>
 					<el-button @click="todoShow=false">X 取消</el-button>
 					<el-button type="primary">+ 新增任務</el-button>
@@ -22,9 +22,11 @@
     </el-form>
     <ul class="lists">
       <li v-for="(item, index) in lists" :key="index" :class="{ 'status_star' : item.status_star }">
-        <el-checkbox :id="'todo-' + index" v-model="item.status_checked">{{ item.title }}</el-checkbox>
-        <el-button type="warning" v-model="item.status_star" :icon="icon" circle @click="onStarSwitch(index)"></el-button>
-        <el-button type="primary" icon="el-icon-edit" circle></el-button>
+        <div class="lists__hr">
+          <el-checkbox :id="'todo-' + index" v-model="item.status_checked">{{ item.title }}</el-checkbox>
+          <el-button type="warning" v-model="item.status_star" :icon="icon" circle @click="onStarSwitch(index)"></el-button>
+          <el-button type="primary" icon="el-icon-edit" circle></el-button>
+        </div>
         <i class="el-icon-alarm-clock">{{ item.deadline | timeStamp | dateFormat('YYYY.MM.DD HH:mm') }}</i>
         <p class="lists__content" v-html="$options.filters.break(item.content)"></p>
       </li>
@@ -66,53 +68,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-.main__input {
-  margin: 24px 0;
-  input {
-    width: calc(100% - 35px);
-		height: 65px;
-		border-radius: 5px;
-		background: #fff;
-		border: 1px solid #c8c8c8;
-		padding-left: 33px;
-		font-size: 24px;
-  }
-}
-
-.main__content {
-  background: #f2f2f2;
-  border: 1px solid #f2f2f2;
-	padding: 24px 72px 0 72px;
-	margin-top: -21px;
-	border-radius: 5px;
-}
-
-.main__action {
-  .el-button-group {
-		width: 100%;
-	}
-	button {
-		width: 50%;
-		border-radius: 0;
-		border: none;
-		padding: 16px 0;
-		font-size: 20px;
-	}
-	.el-button--default {
-		color: #d0021b;
-	}
-	.el-button--primary {
-		background: #4a90e2;
-	}
-}
-
-.el-checkbox__input.is-checked+.el-checkbox__label {
-  text-decoration: line-through;
-}
-
-.status_star {
-  background: #fff2dc;
-}
-</style>
