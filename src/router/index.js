a4f5cdc1dcba17836036661c6a6c9dd40375952d
muki wu <mukispace@gmail.com>
2020-07-17 17:49:14 +0800

    新增 edit 頁面

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Progress from '../views/Progress.vue'
 import Complete from '../views/Complete.vue'
+import Edit from '../views/Edit.vue'
 
 Vue.use(VueRouter)
 
@@ -21,7 +22,12 @@ Vue.use(VueRouter)
     path: '/complete',
     name: 'Complete',
     component: Complete
-  }
+  },
+  {
+    path: '/edit/:id',
+    name: 'Edit',
+    component: Edit
+  },
 ]
 
 const router = new VueRouter({
