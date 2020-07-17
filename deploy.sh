bce2e79d600c81857f53804a458e3e09aa395d2f
muki wu <mukispace@gmail.com>
2020-07-17 17:31:11 +0800

    新增 deploy 設定

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 當發生錯誤時中止腳本
+set -e
+
+# 構建
+npm run build
+
+# cd 到構建輸出的目錄下 
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:mukiwu/vue-todo.git master:gh-pages
+
+cd -
